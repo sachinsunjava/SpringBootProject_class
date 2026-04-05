6abed4d9f85eb08aff6b71fab2c1d44c76dcaea5
Sachin Sun <sachinsun.java@gmail.com>
2026-04-05 18:43:04 +0530

    Loginpage

---
+++ src/main/java/com/ducat/LoginPageUsingMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ducat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPageUsingMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPageUsingMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducat/LoginPageUsingMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageUsingMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
